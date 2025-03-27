e16153c31f72c13bdd8710bb6ea887e3e6ef6e79
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2025-03-27 14:12:20 -0500

    Began working on Terminal Adventure Implementation

---
+++ lost_relic.py
@@ -0,0 +1,10 @@
+while True:
+    intro = "You stand at the Ruins of Eldoria, vines creeping over the ancient" \
+    "stone doorway.\n Legends speak of deadly traps, cryptic puzzles and a relic of" \
+    "immense power.\nWhat do you do next?"
+    print(intro)
+    print('\nEnter "q" at any time to quit.')
+    scenario_1 = int(input('1. Enter the Ruins Cautiously\n'
+    '2. Inspect the Surroundings\n'
+    '3. Turn Back\n'))
+    break
